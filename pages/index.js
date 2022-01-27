6d13139d384dfb8e99fc188df82dad3808e0fd29
Brandon <bmushori@gmail.com>
2022-01-27 11:28:23 +0200

    Graph CMS connection enabled

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {
-    title: "React Testing",
-    excerpt:
-      "React Testing is a collection of React testing tools and libraries.",
-  },
-  { title: "React With Tailwind", excerpt: "Learn Recat with Tailwind CSS." },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -32,4 +24,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
