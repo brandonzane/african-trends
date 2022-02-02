8c9731cbbb0c6558e4c66e698b588d2317e8ed33
Brandon <bmushori@gmail.com>
2022-02-02 23:33:11 +0200

    Footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
+    <div className="mt-12 lg:mt-18 sm:pb-10 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-black">
-        <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
+        <div className="pb-8 mb-2 border-t-2 border-white dark:border-white-300"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
           <p>2022 © African Trends</p>
           <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
