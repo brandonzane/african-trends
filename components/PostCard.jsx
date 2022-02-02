95ee0351fa431830f2e5f0514845d494e2be8690
Brandon <bmushori@gmail.com>
2022-02-02 22:45:09 +0200

    Button bug fix

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -50,7 +50,7 @@ const PostCard = ({ post }) => {
         {post.excerpt}
       </p>
       <div className="text-center">
-        <Link href={`/posts/${post.slug}`}>
+        <Link href={`/post/${post.slug}`}>
           <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-gray-800 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
             Continue Reading
           </span>
