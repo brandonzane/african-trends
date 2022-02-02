24ada99926abafa37b6a35a8261aa5a397373322
Brandon <bmushori@gmail.com>
2022-02-02 14:06:13 +0200

    Carousel Fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,17 +3,11 @@ import Link from "next/link";
 
 import { getCategories } from "../services";
 
-// const categories = [
-//   { name: "React", slug: "react" },
-//   { name: "Web Development", slug: "web-dev" },
-// ];
-
 const Header = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    getCategories()
-      .then((newCategories) => setCategories(newCategories))
+    getCategories().then((newCategories) => setCategories(newCategories));
   }, []);
 
   return (
